ca73ecf03d3654b2fb022de3793c779814eaf697
Md-Abu-Taher-Mazumdar <mdabutaher2002@gmail.com>
2023-04-12 00:36:39 +0600

    first commit

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
-   printf("badsha bai help");
+   printf("badsha bai help\n");
+   printf("I am abu taher mazumdar");
+ printf("I am abu taher mazumdar");
     return 0;
 }
